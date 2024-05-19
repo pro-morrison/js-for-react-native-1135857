ca91a8691a333f3014146e3b876e59a829adb5c6
YasminMorrison <morrisonismailayasmin@gmail.com>
2024-05-19 11:31:04 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -5,3 +5,17 @@ function processArray(arr) {
 
 // Example usage:
 console.log(processArray([5,4,3,7,8,1,6])); 
+
+
+function formatArrayStrings(strings, processedNumbers) {
+    return strings.map((str, index) => {
+       return processedNumbers[index] % 2 === 0 ? str.toUpperCase() : str.toLowerCase();
+     
+    });
+}
+
+// Example usage:
+ const numbers = [5,4,3,7,8,1,6];
+ const strings = ["The", "quick", "brown", "fox", "jumps", "over", "fence"];
+const processedNumbers = processArray(numbers); // [ 15, 16, 9, 21, 64, 3, 36]
+ console.log(formatArrayStrings(strings, processedNumbers));
